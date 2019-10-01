991506c19935cd70386cffcd2445695d4ce456b7
victoresg <victor_esg9@hotmail.com>
2019-09-30 22:27:49 -0300

    Updated todo list.

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="row d-flex justify-content-center notes mb-5 mt-5">
-    <p v-for="(item, index) in companyNotes" :key="index" class="col-md-4 m-2">
-      {{ index }}) {{ item }}
-    <button @click="removeNote">X</button>
-    </p>
+  <div class="notes mb-5 mt-5">
+    <h1 class="mb-5 d-flex justify-content-center">Your notes</h1>
+    <div v-if="!companyNotes.length">
+      <h2 class="d-flex justify-content-center">
+        You have no notes yet!
+      </h2>
+    </div>
+    <div v-else class="row d-flex justify-content-center">
+      <p v-for="(item, index) in companyNotes" :key="index" class="col-md-4 m-2">
+        {{ index }}) {{ item }}
+      <button @click="removeNote">X</button>
+      </p>
+    </div>
   </div>
 </template>
 
